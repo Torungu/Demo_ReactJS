45e33f3590542f4290528f452b9a783f45d61efb
35E_TranThienTrung <xxdarkblade2906@gmail.com>
2022-10-30 20:30:27 +0700

    Demo

--- src/index.js
+++ src/index.js
@@ -13,23 +13,38 @@ import BaitapState from './DemoState/BaiTapXemChiTiet/BaiTapXemChiTiet';
 import BaiTapXemChiTiet from './DemoState/BaiTapXemChiTiet/BaiTapXemChiTiet';
 import CarStore from './Props/CarStore/CarStore';
 import DemoChildren from './Props/DemoChildren/DemoChildren';
-
+//Setup react router dom
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+//JSX: KHÔNG HIỂU
 root.render(
-  <div>
-    {/* <BaitapDemo /> */}
-    {/* <DataBinding /> */}
-    {/* <HandleEvent /> */}
-    {/* <DemoState /> */}
-    {/* <RenderList /> */}
-    {/* <RenderFilm /> */}
-    {/* <DemoProps /> */}
-    {/* <ShoesApp /> */}
-    {/* <BaiTapXemChiTiet /> */}
-    <CarStore />
-    {/* <DemoChildren /> */}
-  </div>
+  // <div>
+  //   {/* <BaitapDemo /> */}
+  //   {/* <DataBinding /> */}
+  //   {/* <HandleEvent /> */}
+  //   {/* <DemoState /> */}
+  //   {/* <RenderList /> */}
+  //   {/* <RenderFilm /> */}
+  //   {/* <DemoProps /> */}
+  //   {/* <ShoesApp /> */}
+  //   {/* <BaiTapXemChiTiet /> */}
+  //   <CarStore />
+  //   {/* <DemoChildren /> */}
+  // </div>
+  <BrowserRouter>
+    <Routes>
+      <Route path="" element={<Home />}>
+        <Route path="home" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="carstore" element={<CarStore />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
